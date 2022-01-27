0a883012d3be54047b9b506f1eb76453150a31ab
unknown <Administrador@MDHYH011-02319.ad.buenosaires.gob.ar>
2022-01-27 16:19:01 -0300

    clase 95

---
+++ src/componentes/Nav.js
@@ -0,0 +1,16 @@
+import "../styles/Nav.css";
+
+const Nav = () => {
+    return (
+        <nav className = "navbar">
+            <ul>
+                <li>Principal</li>
+                <li>Contacto</li>
+                <li>Recursos</li>
+            </ul>
+        </nav>
+    )
+};
+
+
+export default Nav
