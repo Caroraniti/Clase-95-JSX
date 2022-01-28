7e38eba9a8f7bee71bb317879fb3c4ea3f18e5cb
unknown <Administrador@MDHYH011-02319.ad.buenosaires.gob.ar>
2022-01-28 17:30:18 -0300

    quinto commit

--- src/componentes/Card.js
+++ src/componentes/Card.js
@@ -1,12 +1,16 @@
 import "../styles/Card.css"
 
-const Card = () => {
-     return ( <article className = "card">
-            <h2>Tarjeta</h2>
-            <img src= "http://www.placekitten.com/200" />
+const Card = (props) => {
+    return ( <article className = "card">
+            <h2>{props.nombre}</h2>
+            <img src= {props.imagen} />
         </article>
     )
 }
 
 
 export default Card;
+
+
+
+//Los parametros de un componente en react se llaman PROPS (propiedades)
--- src/componentes/Main.js
+++ src/componentes/Main.js
@@ -6,9 +6,23 @@ const Main = () => {
         <main className="main">
             <h1>Bienvenidas a React</h1>
             <div className="tarjetas">
-                <Card />
-                <Card />
-                <Card />
+
+                <Card
+                    nombre="Rita"
+                    imagen="https://estaticos.muyinteresante.es/media/cache/1140x_thumb/uploads/images/gallery/5937e90a5bafe882f5bc09e6/gatitos-cesta_0.jpg"
+                />
+
+                <Card
+                    nombre="Trufa"
+                    imagen="https://www.purina-latam.com/sites/g/files/auxxlc391/files/styles/social_share_large/public/Purina%C2%AE%20La%20llegada%20del%20gatito%20a%20casa.jpg?itok=_3VnSPSl"
+                />
+
+
+                <Card
+                    nombre="Misha"
+                    imagen="https://lokiero.gt/wp-content/uploads/2020/04/11.jpg"
+                />
+
             </div>
 
 
