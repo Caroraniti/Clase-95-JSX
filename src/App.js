a0cb2aefbf5d9eb9559db0cb71146fbbfd745c73
unknown <Administrador@MDHYH011-02319.ad.buenosaires.gob.ar>
2022-01-28 17:55:33 -0300

    sexto commit

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import "./App.css";
 import Nav from "./componentes/Nav";
 import Main from "./componentes/Main";
 import Footer from "./componentes/Footer";
+import Button from "./componentes/Button"
 
 const App = () => {
   return (
     <div className = "contenedor">
       <Nav />
       <Main />
+      <Button mensaje="Hola" color="rojo"/>
+      <Button mensaje="Chau" color="azul"/>
+      <Button mensaje="???" color="violeta" />
       <Footer />
     </div>
   )
--- src/componentes/Card.js
+++ src/componentes/Card.js
@@ -3,6 +3,7 @@ import "../styles/Card.css"
 const Card = (props) => {
     return ( <article className = "card">
             <h2>{props.nombre}</h2>
+            <p>Tarjeta</p>
             <img src= {props.imagen} />
         </article>
     )
